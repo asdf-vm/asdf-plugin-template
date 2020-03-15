e153cddcb1d91a69df43f608af1f07579a45a2d4
Victor Hugo Borja <vborja@apache.org>
2020-03-14 18:56:11 -0600

    shfmt lines

--- template/lib/utils.bash
+++ template/lib/utils.bash
@@ -23,8 +23,8 @@ sort_versions() {
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |\
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |\
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
     sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
