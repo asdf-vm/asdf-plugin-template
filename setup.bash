bee650b74dab3142f6fd4b40012441f22bb7719a
jthegedus <jthegedus@hey.com>
2021-07-05 16:45:06 +1000

    test: revert git rev-parse head

--- setup.bash
+++ setup.bash
@@ -129,7 +129,7 @@ setup_github() {
   license_keyword="${7:-$(ask_license)}"
   license_keyword="$(echo "$license_keyword" | tr '[:upper:]' '[:lower:]')"
 
-  primary_branch="$(git rev-parse --abbrev-ref HEAD)"
+  primary_branch="main"
 
   cat <<-EOF
 Setting up plugin: asdf-$tool_name
