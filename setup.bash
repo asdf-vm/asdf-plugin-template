48dc2f8e84d4fa430a775cb6f5b3226be55d801d
jthegedus <jthegedus@hey.com>
2021-07-05 16:53:44 +1000

    test: change git revparse to hardcoded master

--- setup.bash
+++ setup.bash
@@ -129,7 +129,7 @@ setup_github() {
   license_keyword="${7:-$(ask_license)}"
   license_keyword="$(echo "$license_keyword" | tr '[:upper:]' '[:lower:]')"
 
-  primary_branch="main"
+  primary_branch="master"
 
   cat <<-EOF
 Setting up plugin: asdf-$tool_name
