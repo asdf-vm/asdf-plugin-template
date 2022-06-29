c9a8deda8578c81a77bdb5ecfccd7c05148e94bb
James Hegedus <jthegedus@hey.com>
2022-06-29 22:47:52 +1000

    chore: fix typo in TODO item

--- template/lib/utils.bash
+++ template/lib/utils.bash
@@ -61,7 +61,7 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert <YOUR TOOL> executable exists.
+    # TODO: Assert <YOUR TOOL> executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
