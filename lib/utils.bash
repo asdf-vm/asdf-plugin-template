50c79b916ec7b1c512b075aa7d93519dd791326a
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2023-03-18 00:40:48 +0100

    Debug

--- lib/utils.bash
+++ lib/utils.bash
@@ -81,6 +81,9 @@ install_version() {
     # TODO: Assert okta-aws-cli executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    echo "tool_cmd: ${tool_cmd}"
+
+
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
